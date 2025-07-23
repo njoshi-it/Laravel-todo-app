12645a25023df84ea1d0a0e0d8bf09e60a498c3d
Nabin Joshi <joshinabin792@gmail.com>
2025-07-23 21:25:30 +0545

    task adding done

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+
+    // Allow mass assignment for these fields
+    protected $fillable = ['title', 'is_done'];
 }
