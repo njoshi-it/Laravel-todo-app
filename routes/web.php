62ffcde3a2fc05cb413fb9ee0ee0d0af9111c3cc
Nabin Joshi <joshinabin792@gmail.com>
2025-07-23 21:04:47 +0545

    setup the routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Home page - list all tasks
+Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+
+// Add a new task
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+
+// Toggle task completion
+Route::patch('/tasks/{id}/toggle', [TaskController::class, 'toggle'])->name('tasks.toggle');
+
+// Delete a task
+Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy');
